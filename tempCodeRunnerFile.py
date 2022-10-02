bb79c95e63dceea86752c5c52474073c00c1757e
Shivam Shandilya <70915754+legitShivam@users.noreply.github.com>
2022-10-03 01:16:26 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-except:
-#     si = subprocess.STARTUPINFO()
-#     si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-#     # si.wShowWindow = subprocess.SW_HIDE # default
-#     subprocess.call('taskkill /F /IM chromedriver.exe',
-#                     startupinfo=si)  # Closes the program
